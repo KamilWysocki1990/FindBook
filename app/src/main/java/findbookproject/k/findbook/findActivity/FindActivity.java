beae4eb523f883a39768a7e64178629d2d8b78ed
[DKamilW <kamil.wysocki.1990@gmail.com>
2019-01-01 18:37:59 +0100

    Seting shape, corners and animation for EditText and Search Button

--- app/src/main/java/findbookproject/k/findbook/findActivity/FindActivity.java
+++ app/src/main/java/findbookproject/k/findbook/findActivity/FindActivity.java
@@ -3,12 +3,16 @@ package findbookproject.k.findbook.findActivity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import javax.inject.Inject;
 
+import butterknife.BindAnim;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -29,6 +33,8 @@ public class FindActivity extends AppCompatActivity implements FindActivityContr
     @BindView(R.id.textForShow)
     TextView textView;
 
+    @BindView (R.id.search_button)
+    Button searchButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,6 +50,8 @@ public class FindActivity extends AppCompatActivity implements FindActivityContr
 
     @OnClick(R.id.search_button)
     public void onSearchClick() {
+       final Animation animButton = AnimationUtils.loadAnimation(this,R.anim.bounce);
+        searchButton.startAnimation(animButton);
         presenter.searchForTextOrBook(searchEditText.getText().toString());
     }
 
